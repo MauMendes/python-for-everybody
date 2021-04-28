3dcac76f6014ae9a80689bd6020088c608a9a2fc
Mauricio Mendes <mauricio.eletrica@gmail.com>
2021-04-28 16:07:40 +1200

    Updated notes

--- notes.py
+++ notes.py
@@ -443,3 +443,182 @@
 #        break
 #    print(data.decode())
 #mysock.close()
+
+# ASCII Standards
+# print(ord('\n'))
+
+### NOT AVAILABLE on Python 2
+#import urllib.request, urllib.parse, urllib.error
+#
+#fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+#for line in fhand:
+#    print(line.decode().strip())
+
+#import xml.etree.ElementTree as ET
+#data = '''<person>
+#    <name>Chuck</name>
+#    <phone type="intl">
+#        +1 734 303 4456
+#    </phone>
+#    <email hide="yes"/>
+#</person>'''
+#tree = ET.fromstring(data)
+#print('Name:', tree.find('name').text)
+#print('Attr:', tree.find('email').get('hide'))
+#print(ET.tostring(tree))
+
+
+#import xml.etree.ElementTree as ET
+
+#data = '''<stuff>
+#    <users>
+#        <user x="2">
+#            <id>001</id>
+#            <name>Chuck</name>
+#        </user>
+#        <user x="7">
+#            <id>009</id>
+#            <name>Brent</name>
+#        </user>
+#    </users>
+#</stuff>'''
+
+#stuff = ET.fromstring(data)
+#lst = stuff.findall('users/user')
+#print('User Count:', len(lst))
+#for item in lst:
+#    print('Name', item.find('name').text)
+#    print('Id', item.find('id').text)
+#    print('Attribute', item.get("x"))
+
+#import urllib.request, urllib.parse, urllib.error
+#import xml.etree.ElementTree as ET
+#import ssl
+#
+#api_key = False
+## If you have a Google Places API key, enter it here
+## api_key = 'AIzaSy___IDByT70'
+## https://developers.google.com/maps/documentation/geocoding/intro
+#
+#if api_key is False:
+#    api_key = 42
+#    serviceurl = 'http://py4e-data.dr-chuck.net/xml?'
+#else :
+#    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/xml?'
+#
+## Ignore SSL certificate errors
+#ctx = ssl.create_default_context()
+#ctx.check_hostname = False
+#ctx.verify_mode = ssl.CERT_NONE
+#
+#while True:
+#    address = input('Enter location: ')
+#    if len(address) < 1: break
+#
+#    parms = dict()
+#    parms['address'] = address
+#    if api_key is not False: parms['key'] = api_key
+#    url = serviceurl + urllib.parse.urlencode(parms)
+#    print('Retrieving', url)
+#    uh = urllib.request.urlopen(url, context=ctx)
+#
+#    data = uh.read()
+#    print('Retrieved', len(data), 'characters')
+#    print(data.decode())
+#    tree = ET.fromstring(data)
+#
+#    results = tree.findall('result')
+#    lat = results[0].find('geometry').find('location').find('lat').text
+#    lng = results[0].find('geometry').find('location').find('lng').text
+#    location = results[0].find('formatted_address').text
+#
+#    print('lat', lat, 'lng', lng)
+#    print(location)
+
+#import json
+
+#data = '''{
+#    "name" : "Chuck",
+#    "phone" :{
+#        "type" : "intl",
+#        "number": "+1 734 303 4456"
+#    }
+#    "email" : {
+#        "hide" : "yes"
+#    }
+#}'''
+
+#info = json.loads(data)
+#print('Name:', info["name"])
+#print('Hide:', info["email"])["hide"]
+
+#class PartyAnimal:
+#    x = 0
+#
+#    def __init__(self, x):
+#        print('I am constructed')
+#        self.x = x 
+#
+#    def party(self):
+#        self.x = self.x + 1
+#        print("So Far", self.x)
+#
+#    
+#    def __del__(self):
+#        print('I am desconstructed')
+#
+#an = PartyAnimal(1)
+#
+#an.party()
+#an.party()
+#an.party()
+#an = 42
+#bn = PartyAnimal(0)
+#bn.party()
+
+
+#### DATABASE
+#INSERT INTO Users(name, email) VALUES('Kirsten','kirsten@gmail.com')
+#DELETE FROM Users WHERE email='kirsten@gmail.com'
+#UPDATE Users SET name='Chuck Kirsten' WHERE email='Chuck@chuck.com.br'
+#SELECT * FROM Users
+#SELECT * FROM Users WHERE email='mauricio.eletrica@gmail.com'
+#SELECT * FROM Users ORDER BY email
+#SELECT * FROM Users ORDER BY name
+
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
+#if (len(fname) < 1): fname = 'mbox-short.txt'
+fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1].split('@')
+    org = email[1]
+
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    conn.commit()
+
+# https://www.sqlite.org/lang_select.html
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
